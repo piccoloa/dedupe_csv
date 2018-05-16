b3b2d192184efd9ab90244277c6182e12770f7b7
piccoloa <alexpiccolo@gmail.com>
2018-05-16 17:00:45 -0400

    Sych

--- DockerBuildJupyterWithDedupe/start_dedupe_notebook.sh
+++ DockerBuildJupyterWithDedupe/start_dedupe_notebook.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -it --network my-network --hostname dedupe -e GRANT_SUDO=yes --user root --name=alexDedupe -p 8888:8888 -v /Users/alessandropiccolo/github/dedupe_csv/notebooks:/work dedupe
+docker run --rm -it --network my-network --hostname dedupe -e GRANT_SUDO=yes --user root --name=alexDedupe -p 8888:8888 -v /Users/alessandropiccolo/github/dedupe_csv/notebooks:/home/jovyan/work dedupe
